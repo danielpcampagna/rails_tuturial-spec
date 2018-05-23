92690b44091720785a68178788af041a02afe051
Daniel Campagna <danielcampagna@id.uff.br>
2018-05-23 15:37:27 -0300

    fix tests

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -48,31 +48,32 @@ describe Contact do
   # Testing class methods and scopes
 
   describe "filter last name by letter" do
-    @before :each do
-      smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    @jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    @johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-  end
-  context "with matching letters" do
-    it "returns a sorted array of result that match" do
-      expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+    before :each do
+      @smith = Contact.create(
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'jsmith@example.com'
+      )
+      @jones = Contact.create(
+        firstname: 'Tim',
+        lastname: 'Jones',
+        email: 'tjones@example.com'
+      )
+      @johnson = Contact.create(
+        firstname: 'John',
+        lastname: 'Johnson',
+        email: 'jjohnson@example.com'
+      )
     end
-  end
-  context "with non-matching letters" do
-    it "omits results that do not match" do
-      expect(Contact.by_letter("J")).not_to include @smith
+    context "with matching letters" do
+      it "returns a sorted array of result that match" do
+        expect(Contact.by_letter("J")).to eq [@johnson, @jones]
+      end
     end
-  end  
+    context "with non-matching letters" do
+      it "omits results that do not match" do
+        expect(Contact.by_letter("J")).not_to include @smith
+      end
+    end  
+  end
 end
